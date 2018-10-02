8d4eed6c9ce1ba53bd55cce3f63739aac1ffe60d
Niwesh Gupta <niweshgupta@gmail.com>
2018-10-02 22:09:58 +0530

    Singly linked list added

---
+++ singly_linked_list.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list {
+  int data;
+  struct list* next;
+}node;
+
+int total = 0;
+node *head;
+node *Insert_Begin(int data) {
+  total++;
+  node* temp = malloc (sizeof(node));
+  temp -> data = data;
+  temp -> next = head;
+  return temp;
+}
+node *Insert_End(int data) {
+  total++;
+  node* temp = malloc (sizeof(node));
+  temp -> data = data;
+  temp -> next = NULL;
+  if (head == NULL)
+    return temp;
+  node* q = head;
+  while (q -> next != NULL) {
+    q = q -> next;
+  }
+  q -> next = temp;
+  return head;
+}
+node *Insert (int data, int pos) {
+  if (pos == 0){
+    total++;
+    return Insert_Begin(data);
+  }
+  if (pos == total){
+    total++;
+    return Insert_End(data);
+  }
+  if (pos > total) {
+    printf("position out of bound\n");
+    return head;
+  }
+  total++;
+  node* temp = malloc (sizeof(node));
+  temp -> data = data;
+  int count = 0;
+  node *q = head;
+  while (count < pos - 1) {
+    count++;
+    q = q -> next;
+  }
+  temp -> next = q -> next;
+  q -> next = temp;
+  return head;
+}
+node *Delete_Beg () {
+  if (head == NULL)  {
+    printf("Nothing to Delete\n");
+    return head;
+  }
+  total--;
+  if (head -> next == NULL){
+    free (head);
+    return NULL;
+  }
+  node *q = head;
+  head = head -> next;
+  free (q);
+  return head;
+}
+node *Delete_End () {
+  if (head == NULL)  {
+    printf("Nothing to Delete\n");
+    return head;
+  }
+  total--;
+  if (head -> next == NULL){
+    free (head);
+    return NULL;
+  }
+  if ((head -> next) -> next == NULL) {
+    free (head -> next);
+    head -> next = NULL;
+    return head;
+  }
+  node *q = head;
+  while (q -> next -> next != NULL) {
+    q = q -> next;
+  }
+  free (q -> next);
+  q -> next = NULL;
+  return head;
+}
+node *Delete (int pos) {
+  if (pos == 0)
+    return Delete_Beg();
+  if (pos == total - 1){
+    return Delete_End();
+  }
+  if (pos > total) {
+    printf("position out of bound\n");
+    return head;
+  }
+  total--;
+  int count = 0;
+  node *q = head, *temp;
+  while (count < pos - 1) {
+    count++;
+    q = q -> next;
+  }
+  temp = q -> next;
+  q -> next = temp -> next;
+  free (temp);
+  return head;
+}
+void Display () {
+  printf("No of nodes : %d\n", total);
+  if (head == NULL) {
+    printf("Nothing To Display\n");
+    return;
+  } 
+  node *p = head;
+  while (p != NULL) {
+    printf("%d ", p -> data);
+    p = p -> next;
+  }
+  printf("\n");
+}
+
+int main() {
+  int data, choice, pos;
+  head = malloc(sizeof(head));
+  head = NULL;
+  while (1) {
+    printf("1.Insert at beginning\n2.Insert at end\n3.Insert at Intermediate\n");
+    printf("4.Delete at beginning\n5.Delete at end\n6.Delete at Intermediate\n7.Display\n");
+    scanf("%d", &choice);
+    switch (choice) {
+      case 1:
+        printf("Enter the data to Insert : ");
+        scanf ("%d", &data);
+        head = Insert_Begin(data);
+        break;
+      case 2:
+        printf("Enter the data to Insert : ");
+        scanf ("%d", &data);
+        head = Insert_End(data);
+        break;
+      case 3:
+        printf("Enter the position (0 - based indexing) : ");
+        scanf ("%d", &pos);
+        printf("Enter the data to Insert : ");
+        scanf ("%d", &data);
+        head = Insert(data, pos);
+        break;
+      case 4:
+        head = Delete_Beg();
+        break;
+      case 5:
+        head = Delete_End();
+        break;
+      case 6:
+        printf("Enter the position (0 - based indexing) : ");
+        scanf ("%d", &pos);
+        head = Delete(pos);
+        break;
+      case 7:
+        Display();
+        break;
+      default :
+        return 0;
+    }
+  }
+}
